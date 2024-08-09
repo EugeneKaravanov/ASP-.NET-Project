45b70dd348294bfb47781b08b5a9244eea813221
EugeneKaravanov <karavanoveugene@gmail.com>
2024-08-09 15:43:25 +0300

    Commit #7.2 (Исправлена интеграция Swagger)

--- PET ASP .NET Project/GatewayService/Program.cs
+++ PET ASP .NET Project/GatewayService/Program.cs
@@ -5,13 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 var address = builder.Configuration.GetValue<string>("ProductServiceAddress");
 
 builder.Services.AddGrpcClient<Ecommerce.ProductService.ProductServiceClient>(address, options => { options.Address = new Uri(address); });
-//builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-//app.MapControllers();
 
 if (app.Environment.IsDevelopment())
 {
@@ -21,5 +19,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseRouting();
 app.UseMiddleware<RequestLoggingMiddleware>();
+app.MapControllers();
 app.Run();
 
