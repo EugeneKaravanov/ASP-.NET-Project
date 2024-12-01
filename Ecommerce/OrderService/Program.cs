12068888619e9d0ce8e0bad872bad36b2236dd17
EugeneKaravanov <karavanoveugene@gmail.com>
2024-12-01 22:39:13 +0300

    Commit #6 (FluentMigration для OrderService)

--- Ecommerce/OrderService/Program.cs
+++ Ecommerce/OrderService/Program.cs
@@ -1,6 +1,28 @@
+using System.Data;
+using FluentMigrator.Runner;
+using Npgsql;
+using OrderService.Migrations;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddScoped<IDbConnection>(_ =>
+{
+    return new NpgsqlConnection(connectionString);
+});
+
+builder.Services.AddFluentMigratorCore()
+    .ConfigureRunner(rb => rb
+        .AddPostgres()
+        .WithGlobalConnectionString(builder.Configuration.GetConnectionString("DefaultConnection"))
+        .ScanIn(typeof(InitialMigrationForOrders).Assembly).For.Migrations());
+
 var app = builder.Build();
 
-
+using (var scope = app.Services.CreateScope())
+{
+    var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+    runner.MigrateUp();
+}
 
 app.Run();
